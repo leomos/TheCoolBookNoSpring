28d6b6f9eac4a3eb392ca7723a43b3d27efd0ee0
Administrator <Administrator@192.168.20.54>
2019-10-15 14:46:53 +0200

    ReCommit

--- src/com/etlforma/servlet/allEvents.java
+++ src/com/etlforma/servlet/allEvents.java
@@ -13,35 +13,30 @@ import javax.servlet.http.HttpServletResponse;
 import com.etlforma.db.Database;
 import com.etlforma.model.Event;
 
-
 /**
  * Servlet implementation class allEvents
  */
 @WebServlet("/allEvents")
 public class allEvents extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       private Database db;
-    
-      @Override
-     
-       public void init (ServletConfig config) throws ServletException {
-    	   super.init(config);
-    	   db = new Database();
-    	   
-       }
-
+	private Database db;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		db = new Database();
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		List<Event> events = db.getEvents();
-		
+
 		request.setAttribute("events", events);
-		
+
 		request.getRequestDispatcher("/allEvents.jsp").forward(request, response);
-}
-
-
-
+	}
 
 }
